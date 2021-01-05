5db3e42ed5e4e06ec275b250df2eb4229f4335ad
komal88888 <komalsony88888@gamil.com>
2021-01-06 00:24:49 +0530

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void browserRestAPI()
+	{
+		System.out.println("browserRestAPI");
+	}
+	
+	@Test
+	public void elementUIRestAPI()
+	{
+		System.out.println("elementUIRestAPI");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserSelenium()
+	{
+		System.out.println("browserSelenium");
+	}
+	
+	@Test
+	public void elementUISelenium()
+	{
+		System.out.println("elementUISelenium");
+	}
+
+
+}
